ff45968580d5c47a471139aeb45be018eb7bf490
gazi04 <gazmendhalili2016@gmail.com>
2025-10-12 21:32:44 +0200

    :sparkles: Add new features for email interaction through gmail
    This commit adds new endpoints that can send email messages and create
    drafts

---
+++ backend/actions/routes/action_router.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from actions.services.gmail_service import GmailService
+from auth.depedencies import get_current_user
+from core.database import get_db
+from user.models.user import User
+
+
+action_router = APIRouter(
+    prefix="/action",
+    tags=["Action"]
+)
+
+@action_router.get("/create_draft")
+async def create_draft_with_gmail(db: Session = Depends(get_db), user: User = Depends(get_current_user)):
+    return GmailService.create_draft(db, user.id)
+
+@action_router.get("/send_email")
+async def send_mail_in_gmail(db: Session = Depends(get_db), user: User = Depends(get_current_user)):
+    return GmailService.send_message(db, user.id)
