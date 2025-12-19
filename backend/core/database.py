95ce0eb08e54c49f1e51066cf73aa6758f66e922
gazi04 <gazmendhalili2016@gmail.com>
2025-12-19 17:18:46 +0100

    :recycle: Add context manager to get_db() method, and handling exceptions

--- backend/core/database.py
+++ backend/core/database.py
@@ -1,6 +1,10 @@
+from contextlib import contextmanager
+from typing import Iterator
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+
+from core.setup_logging import setup_logger
 from .config_loader import settings
 
 
@@ -11,9 +15,14 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 
-def get_db():
+@contextmanager
+def get_db() -> Iterator[Session]:
     db = SessionLocal()
     try:
         yield db
+    except Exception as e:
+        db.rollback()
+        setup_logger("core/database.py").error(f"Unhandled error occurred: \n{e}")
+        raise e
     finally:
         db.close()
