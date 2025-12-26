73330329338e51ffb525695b1f2e8b760163d2ca
gazi04 <gazmendhalili2016@gmail.com>
2025-12-26 18:25:28 +0100

    :wastebasket: Removed deprecated code

--- backend/actions/services/gmail_service.py
+++ backend/actions/services/gmail_service.py
@@ -1,4 +1,3 @@
-from email.message import EmailMessage
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from uuid import UUID
@@ -9,102 +8,10 @@ from auth.services.auth_service import AuthService
 from core.config_loader import settings
 from user.services.user_service import UserService
 
-import base64
 import logging
 
 
 class GmailService:
-    # 🔴 todo: remove the create_draft and send_message methods from the service
-    # because the code is in the orchestration directory
-    @staticmethod
-    async def create_draft(db: Session, user_id: UUID):
-        """Create and insert a draft email.
-        Print the returned draft's message and id.
-        Returns: Draft object, including draft id and message meta data.
-        """
-
-        provider = "google"
-        scopes = [
-            "https://www.googleapis.com/auth/gmail.send",
-            "https://www.googleapis.com/auth/gmail.compose",
-        ]
-
-        creds = AuthService.get_google_credentials(db, user_id, provider, scopes)
-
-        try:
-            # ♻️ todo: refactore the service names and use enums
-            service = build("gmail", "v1", credentials=creds)
-
-            message = EmailMessage()
-
-            message.set_content("This is automated draft mail")
-
-            message["To"] = "gazmend.halili.st@uni-gjilan.net"
-            message["From"] = "gazmendhalili2016@gmail.com"
-            message["Subject"] = "Automated draft"
-
-            # encoded message
-            encoded_message = base64.urlsafe_b64encode(message.as_bytes()).decode()
-
-            create_message = {"message": {"raw": encoded_message}}
-
-            # pylint: disable=E1101
-            draft = (
-                service.users()
-                .drafts()
-                .create(userId="me", body=create_message)
-                .execute()
-            )
-
-            print(f"Draft id: {draft['id']}\nDraft message: {draft['message']}")
-        except HttpError as error:
-            print(f"An error occurred: {error}")
-            draft = None
-
-        return draft
-
-    @staticmethod
-    async def send_message(db: Session, user_id: UUID):
-        """Create and send an email message
-        Print the returned  message id
-        Returns: Message object, including message id
-        """
-        provider = "google"
-        scopes = [
-            "https://www.googleapis.com/auth/gmail.send",
-            "https://www.googleapis.com/auth/gmail.compose",
-        ]
-
-        creds = AuthService.get_google_credentials(db, user_id, provider, scopes)
-
-        try:
-            service = build("gmail", "v1", credentials=creds)
-            message = EmailMessage()
-
-            message.set_content("This is automated draft mail")
-
-            message["To"] = "gazmend.halili.st@uni-gjilan.net"
-            message["From"] = "gazmendhalili2016@gmail.com"
-            message["Subject"] = "Sending automated email"
-
-            # encoded message
-            encoded_message = base64.urlsafe_b64encode(message.as_bytes()).decode()
-
-            create_message = {"raw": encoded_message}
-            # pylint: disable=E1101
-            send_message = (
-                service.users()
-                .messages()
-                .send(userId="me", body=create_message)
-                .execute()
-            )
-            print(f"Message Id: {send_message['id']}")
-        except HttpError as error:
-            print(f"An error occurred: {error}")
-            send_message = None
-
-        return send_message
-
     @staticmethod
     async def watch_mailbox_for_updates(
         db: Session, user_id: UUID, label_ids: list = ["INBOX"]
