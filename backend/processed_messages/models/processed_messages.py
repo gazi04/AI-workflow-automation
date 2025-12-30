b81c24766b433be4b761cc49c91b2c95bbea79b0
gazi04 <gazmendhalili2016@gmail.com>
2025-12-30 18:44:13 +0100

    :card_file_box: Created new table processed_messages, with ORM model, migration file and it's own module in the project

---
+++ backend/processed_messages/models/processed_messages.py
@@ -0,0 +1,19 @@
+from datetime import datetime, timezone
+from uuid import uuid4
+from sqlalchemy import DateTime, String, null
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import Mapped, mapped_column
+from core.database import Base
+
+
+class ProcessedMessages(Base):
+    __tablename__ = "processed_messages"
+
+    id: Mapped[UUID] = mapped_column(
+        UUID(as_uuid=True), primary_key=True, default=uuid4
+    )
+    message_id: Mapped[str] = mapped_column(String(), unique=True, nullable=False)
+    workflow_id: Mapped[UUID] = mapped_column(UUID(), unique=True, nullable=False)
+    processed_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), default=lambda: datetime.now(timezone.utc)
+    )
