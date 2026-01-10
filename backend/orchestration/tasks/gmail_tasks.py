a16e989bd7d883f78a62549f3e657d5221fc41ba
gazi04 <gazmendhalili2016@gmail.com>
2026-01-10 14:16:24 +0100

    :recycle: Updated the create draft task to be compatible and usable in the end product

--- backend/orchestration/tasks/gmail_tasks.py
+++ backend/orchestration/tasks/gmail_tasks.py
@@ -17,7 +17,7 @@ logger = setup_logger("Prefect Gmail Task")
 
 class GmailTasks:
     @staticmethod
-    async def create_draft(db: Session, user_id: UUID):
+    async def create_draft(user_id: UUID, to: str, subject: str, body: str):
         """Create and insert a draft email.
         Print the returned draft's message and id.
         Returns: Draft object, including draft id and message meta data.
@@ -29,28 +29,23 @@ class GmailTasks:
             "https://www.googleapis.com/auth/gmail.compose",
         ]
 
-        creds = AuthService.get_google_credentials(db, user_id, provider, scopes)
+        with db_session() as db:
+            creds = AuthService.get_google_credentials(db, user_id, provider, scopes)
+            user_email = await UserService.get_email(db, user_id)
 
         try:
-            # ♻️ todo: refactore the service names and use enums
             with build("gmail", "v1", credentials=creds) as service:
-                # service = build("gmail", "v1", credentials=creds)
-
                 message = EmailMessage()
+                message.set_content(body)
 
-                message.set_content("This is automated draft mail")
+                message["To"] = to
+                message["From"] = user_email
+                message["Subject"] = subject
 
-                # todo: remove and pass down the message attributes
-                message["To"] = "gazmend.halili.st@uni-gjilan.net"
-                message["From"] = "gazmendhalili2016@gmail.com"
-                message["Subject"] = "Automated draft"
-
-                # encoded message
                 encoded_message = base64.urlsafe_b64encode(message.as_bytes()).decode()
 
                 create_message = {"message": {"raw": encoded_message}}
 
-                # pylint: disable=E1101
                 draft = (
                     service.users()
                     .drafts()
