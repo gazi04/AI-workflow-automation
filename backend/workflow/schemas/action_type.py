9ef00704a5c4146a75c485f442ff9e610d804500
gazi04 <gazmendhalili2016@gmail.com>
2026-01-26 21:42:26 +0100

    :sparkles: Added smart draft option in the action type

--- backend/workflow/schemas/action_type.py
+++ backend/workflow/schemas/action_type.py
@@ -6,4 +6,5 @@ class ActionType(str, Enum):
     SEND_EMAIL = "send_email"
     REPLY_EMAIL = "reply_email"
     LABEL_EMAIL = "label_email"
+    SMART_DRAFT = "smart_draf"
     CREATE_DOCUMENT = "create_document"
