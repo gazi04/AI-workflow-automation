b98281b2914dcf70b1d30ab405aed7ef878c0c7d
gazi04 <gazmendhalili2016@gmail.com>
2026-01-26 22:06:46 +0100

    :pencil2: Fixed typo

--- backend/workflow/schemas/action_type.py
+++ backend/workflow/schemas/action_type.py
@@ -6,5 +6,5 @@ class ActionType(str, Enum):
     SEND_EMAIL = "send_email"
     REPLY_EMAIL = "reply_email"
     LABEL_EMAIL = "label_email"
-    SMART_DRAFT = "smart_draf"
+    SMART_DRAFT = "smart_draft"
     CREATE_DOCUMENT = "create_document"
