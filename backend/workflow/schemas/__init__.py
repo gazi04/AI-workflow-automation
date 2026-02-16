37ce38e5c15a8a45278c2a0ac4e2769d42bdc6f3
gazi04 <gazmendhalili2016@gmail.com>
2026-02-16 20:26:40 +0100

    :recycle: Removed ActionType and TriggerType

--- backend/workflow/schemas/__init__.py
+++ backend/workflow/schemas/__init__.py
@@ -1,16 +1,12 @@
 from .action import Action
-from .action_type import ActionType
 from .trigger import Trigger
-from .trigger_type import TriggerType
 from .workflow_definition import WorkflowDefinition
 from .toggle_workflow_request import ToggleWorkflowRequest
 from .update_workflow_request import UpdateWorkflowRequest
 
 __all__ = [
     "Action",
-    "ActionType",
     "Trigger",
-    "TriggerType",
     "WorkflowDefinition",
     "ToggleWorkflowRequest",
     "UpdateWorkflowRequest",
