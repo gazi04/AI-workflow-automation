7916d099b43a438f6993d1c69fd389bb31227707
gazi04 <gazmendhalili2016@gmail.com>
2026-03-01 20:13:30 +0100

    :package: Updated the service package __init__ file importing AccountService into __all__

--- backend/auth/services/__init__.py
+++ backend/auth/services/__init__.py
@@ -1,4 +1,5 @@
+from .account_service import AccountService
 from .auth_service import AuthService
 from .token_service import TokenService
 
-__all__ = ["AuthService", "TokenService"]
+__all__ = ["AccountService", "AuthService", "TokenService"]
