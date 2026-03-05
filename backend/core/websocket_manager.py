bdb212107eec15bee6c43232a1f6d3bcd2673e33
gazi04 <gazmendhalili2016@gmail.com>
2026-03-05 20:20:08 +0100

    :sparkles: Creating a connection manager to setup and keep a websocket connection

---
+++ backend/core/websocket_manager.py
@@ -0,0 +1,28 @@
+from typing import List, Dict
+from fastapi import WebSocket
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: Dict[str, List[WebSocket]] = {}
+
+    async def connect(self, user_id: str, websocket: WebSocket):
+        await websocket.accept()
+        if user_id not in self.active_connections:
+            self.active_connections[user_id] = []
+        self.active_connections[user_id].append(websocket)
+
+    def disconnect(self, user_id: str, websocket: WebSocket):
+        if user_id in self.active_connections:
+            self.active_connections[user_id].remove(websocket)
+            if not self.active_connections[user_id]:
+                del self.active_connections[user_id]
+
+    async def send_personal_message(self, message: any, websocket: WebSocket):
+        await websocket.send_json(message)
+
+    async def broadcast_to_user(self, user_id: str, message: any):
+        if user_id in self.active_connections:
+            for connection in self.active_connections[user_id]:
+                await connection.send_json(message)
+
+manager = ConnectionManager()
