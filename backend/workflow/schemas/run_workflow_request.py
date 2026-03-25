4b46e279d17fb3bc117376111f896dc3738975ee
gazi04 <gazmendhalili2016@gmail.com>
2026-03-25 14:23:05 +0100

    :bug: Fixing 422 validation error, by adding a default value the config is not required by the API

--- backend/workflow/schemas/run_workflow_request.py
+++ backend/workflow/schemas/run_workflow_request.py
@@ -4,4 +4,4 @@ from pydantic import BaseModel, Field
 
 class RunWorkflowRequest(BaseModel):
     deployment_id: UUID = Field(..., description="The Prefect Deployment ID")
-    config: dict = Field(..., description="Workflow parameters")
+    config: dict = Field(default_factory=dict, description="Workflow parameters")
