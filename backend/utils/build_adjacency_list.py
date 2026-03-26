9c9d038e67c9975e541111226e82c3036d49d57f
gazi04 <gazmendhalili2016@gmail.com>
2026-03-26 09:44:49 +0100

    :sparkles: Added new util function to optimize adjacency list inside the BFS loop in the master flow

---
+++ backend/utils/build_adjacency_list.py
@@ -0,0 +1,11 @@
+from typing import Dict, List
+from workflow.schemas.edges import Edge
+
+def build_adjacency_list(edges: List[Edge]) -> Dict[str, List[Edge]]:
+    """Maps a Node ID to a list of its outgoing edges for O(1) lookups."""
+    adj_list = {}
+    for edge in edges:
+        if edge.source not in adj_list:
+            adj_list[edge.source] = []
+        adj_list[edge.source].append(edge)
+    return adj_list
