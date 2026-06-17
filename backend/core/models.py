3ef40b8c1832830c1cc9b016673e41c4e39bfdc5
gazi04 <gazmendhalili2016@gmail.com>
2026-06-17 16:04:11 +0200

    :bug: Including the AuthCode model in the import statement

--- backend/core/models.py
+++ backend/core/models.py
@@ -5,7 +5,7 @@ And can be used in the case where we want to import all the models but at the sa
 
 from core.database import Base  # noqa: F401
 
-from auth.models import ConnectedAccount, RefreshToken, OAuthState  # noqa: F401
+from auth.models import ConnectedAccount, RefreshToken, OAuthState, AuthCode  # noqa: F401
 from processed_messages.models import ProcessedMessages  # noqa: F401
 from user.models.user import User  # noqa: F401
 from workflow.models.workflow import Workflow  # noqa: F401
